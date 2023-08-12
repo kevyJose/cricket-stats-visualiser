5a0c6282485dc83626076af61d9f1b5e7d7e266b
Kevin Jose <josekevi@myvuw.ac.nz>
2023-08-12 15:09:08 +1200

    Form submission works now. The data is sent to JS script, is extracted, and is processed for chart configuration.

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -11,9 +11,6 @@ let raw_data;
 
 //Do these initial calls once the DOM is finished loading
 window.addEventListener('DOMContentLoaded', function() {
-  // setupGlobalListeners();
-  // processFormSubmission();  
-
   readData('./data/data-statsguru-3.csv', '#raw')    
     .then(console.log('Data read successfully'))
     .catch((error) => console.log('Error: ', error.message)); 
@@ -21,56 +18,44 @@ window.addEventListener('DOMContentLoaded', function() {
 
 
 
-
-function processFormSubmission(event) {
+const processFormSubmission = (event) => {
   event.preventDefault();
   const form = document.querySelector("form")
-  const log = document.querySelector("#log")
+  // const log = document.querySelector("#log")
   const map = new Map(); // Creating a map to store the data
+  // event.preventDefault();
+  const data = new FormData(form)
 
-  form.addEventListener(
-    "submit",
-    (event) => {
-      // event.preventDefault();
+  for (const entry of data) {
+    const inputElement = form.elements[entry[0]];
+    let key = entry[0]
+    let value = entry[1]
+    console.log('key: ', key)
+    console.log('value: ', value)
 
-      const data = new FormData(form)
+    // radio btn or checkbox
+    if ((inputElement.type === "radio" || inputElement.type === "checkbox") &&
+      inputElement.checked) {
+      map.set(key, value)
+    }
+    // select dropdown 
+    else if (inputElement.tagName === "SELECT") {
+      const selectedOption = inputElement.options[inputElement.selectedIndex];
+      map.set(key, selectedOption.value)
+    }
+    //other input types 
+    else {
+      map.set(key, value)
+    }
+  }
+  // Displaying the map in the log
+  // log.innerText = JSON.stringify(map, null, 2)    
+  // event.preventDefault()
 
-      for (const entry of data) {
-        const inputElement = form.elements[entry[0]];
-        let key = entry[0]
-        let value = entry[1]
-        console.log('key: ', key)
-        console.log('value: ', value)
-
-        // radio btn or checkbox
-        if ((inputElement.type === "radio" || inputElement.type === "checkbox") &&
-          inputElement.checked) {
-          map.set(key, value)
-        }
-        // select dropdown 
-        else if (inputElement.tagName === "SELECT") {
-          const selectedOption = inputElement.options[inputElement.selectedIndex];
-          map.set(key, selectedOption.value)
-        }
-        //other input types 
-        else {
-          map.set(key, value)
-        }
-      }
-      // Displaying the map in the log
-      log.innerText = JSON.stringify(map, null, 2)    
-      // event.preventDefault()
-
-      //set global variables
-      setCurrChartAttributes(map);
-
-      //use extracted info to plot chart
-      doGlobalChart(event, '#scatter_plot');
-    },
-    false
-  );
-
-
+  //set global variables
+  setCurrChartAttributes(map);
+  //use extracted info to plot chart
+  doGlobalChart(event, '#scatter_plot');
   allMaps.push(map)
 }
 
@@ -83,7 +68,7 @@ function setCurrChartAttributes(map) {
   y_title = capitalizeString(y_selected)
   color_code = map.get('color-code')
 
-  print('testing global var updates:  { ', selectedLocation, x_selected, y_selected, x_title, y_title, color_code, ' }')
+  console.log('testing global var updates:  { ', selectedLocation, x_selected, y_selected, x_title, y_title, color_code, ' }')
 }
 
 
@@ -152,55 +137,3 @@ function doGlobalChart(event, id_tag) {
 function capitalizeString(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
-
-
-
-
-// function handleLocationChange(input_elem) {
-//   selectedLocation = input_elem.value
-//   console.log('selected location: ' + selectedLocation)  
-// }
-
-// function handleXAttributeChange(input_elem) {
-//   const selected_x_option = input_elem.options[input_elem.selectedIndex]
-//   x_title = selected_x_option.textContent
-//   x_selected = selected_x_option.value
-//   console.log('selected x-attribute: ' + x_selected)
-//   // console.log('X title: ' + x_title)
-// }
-
-// function handleYAttributeChange(input_elem) {
-//   const selected_y_option = input_elem.options[input_elem.selectedIndex]
-//   y_title = selected_y_option.textContent
-//   y_selected = selected_y_option.value
-//   console.log('selected y-attribute: ' + y_selected)
-//   // console.log('Y title: ' + y_title)
-// }
-
-// function handleColorCodeChange(input_elem) {
-//   const selected_colorCode = input_elem
-//   colorCode_title = selected_colorCode.textContent;
-//   color_code = selected_colorCode.value;
-//   console.log('selected color-code: ' + color_code)
-// }
-
-
-// function setupGlobalListeners() {   
-//   const location_input = document.getElementById('location-menu')
-//   const Xattr_input = document.getElementById('x-attr-menu')
-//   const Yattr_input = document.getElementById('y-attr-menu')
-//   const colorCode_input = document.querySelector('[name="colorCode"]');
-
-//   location_input.addEventListener('change', function() {
-//     handleLocationChange(location_input);
-//   });
-//   Xattr_input.addEventListener('change', function() {
-//     handleXAttributeChange(Xattr_input);
-//   });
-//   Yattr_input.addEventListener('change', function() {
-//     handleYAttributeChange(Yattr_input);
-//   });
-//   colorCode_input.addEventListener('click', function() {
-//     handleColorCodeChange(colorCode_input);
-//   });  
-// }
